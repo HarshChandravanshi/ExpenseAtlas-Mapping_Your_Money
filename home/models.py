a53563b428db07c336fbe8d38f28b6cae4a076d1
Harsh Kumar Chandravanshi <90752233+HarshChandravanshi@users.noreply.github.com>
2023-05-19 18:43:12 +0530

    Update models.py

--- home/models.py
+++ home/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils.timezone import now
 from django.contrib.auth.models import User
 from django.conf import settings
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save 
 from django.dispatch import receiver
 from django.db.models import Sum
 #Create your models here.
